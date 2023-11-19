208fc3f371baf17fe81c9cbea3e215fc7ea42eac
RawadAbdallah <rawad0ne@gmail.com>
2023-11-19 15:16:37 +0200

    Co-authored-by: Abed Al Ghani Shaaban <abedshaaban600@gmail.com> Co-authored-by: Mohammad Khawaja <MoeKhaweja@users.noreply.github.com> Co-authored-by: Emad Hmady <emadhmadyy@users.noreply.github.com>

---
+++ src/components/section/index.tsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+function index() {
+  return <div></div>
+}
+
+export default index
---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+function index() {
+  return <button>index</button>
+}
+
+export default index
