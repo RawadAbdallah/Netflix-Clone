55574bd3ce11045ded9080b32282c17aab13b16a
RawadAbdallah <rawad0ne@gmail.com>
2023-11-19 19:51:12 +0200

    [core] Section component skeleton

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,7 +1,27 @@
 import './index.css'
 
-function index() {
-  return <div></div>
+interface myProps {
+  title: string
+  description: string
+  imgSrc: string
+  isReversed?: false
+}
+
+const index: React.FC<myProps> = ({ title, description, imgSrc, isReversed }) => {
+  const sectionClassName: string = 'section-container ' + (isReversed ? 'reverse' : '')
+
+  return (
+    <section className={sectionClassName}>
+      <div className="section-left">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+
+      <div className="section-right">
+        <img src={imgSrc} />
+      </div>
+    </section>
+  )
 }
 
 export default index
