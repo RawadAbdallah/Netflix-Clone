ee12ca17c83769886db5d6ffef0929c7771df69b
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2023-11-19 23:00:28 +0200

    [clean-up]: checkup and cleanup App

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import Section from '@/components/section'
 import Button from '@/components/ui/button'
 
 function App() {
-  return (
-    <>
-      <Button buttonText="hello world" buttonType="primary" buttonSize="small"></Button>
-      <Button buttonText="hello world" buttonType="primary" buttonSize="medium"></Button>
-      <Button buttonText="hello world" buttonType="primary" buttonSize="large"></Button>
-      <Button buttonText="hello world" buttonType="secondary" buttonSize="large"></Button>
-      <Button
-        buttonText="hello world"
-        buttonType="secondary"
-        buttonSize="medium"
-      ></Button>
-      <Button buttonText="hello world" buttonType="secondary" buttonSize="small"></Button>
-    </>
-  )
+  return <></>
 }
 
 export default App
