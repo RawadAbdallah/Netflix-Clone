68b152348d54517ecc19ea9e9454d8562b5b06d6
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-20 23:33:34 +0200

    [clean-up]: removed unused code run format

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import Header from '@/components/header'
-
-import Button from '@/components/ui/button'
-
 function App() {
-  return (
-    <>
-      <Header>
-        <Button>JOIN NOW</Button>
-        <Button variant="secondary">Sign In</Button>
-      </Header>
-    </>
-  )
+  return <></>
 }
 
 export default App
