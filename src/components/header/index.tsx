6fbd959f8cc1cff0a9432b098b0bd444193c9b96
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-20 23:13:04 +0200

    [style]: added logo to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,10 @@
+import './index.css'
+
 export default function Index() {
-  return <header></header>
+  return (
+    <header className="header">
+      <img src="/logo.png" alt="netflix text logo" className="header-img-logo-text" />
+      <img src="/logo-n.png" alt="netflix logo" className="header-img-logo-n" />
+    </header>
+  )
 }
