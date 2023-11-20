bf7b30216e99756d71402791e07e054e326bb3c6
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2023-11-20 16:10:36 +0200

    [docs]:added button documentation

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,5 +1,17 @@
 import './index.css'
 
+/**
+ * `Button()` returns a button component.
+ *
+ * It takes variant, children, className as parameters.
+ * @param variant ,by default set to primary, can take 'primary' | 'secondary' variants
+ * @param children ,are children passed to the function
+ * @param className ,are additional classes that can be assigned to the component
+ *
+ * @returns a customized button jsx tag
+ *
+ *
+ */
 type ButtonProps = {
   variant?: 'primary' | 'secondary'
   children: React.ReactNode
