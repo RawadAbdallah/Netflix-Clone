f58e830251eee2768dcecd285b27fe949dc6f41b
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-20 23:31:55 +0200

    [docs]: added documentation to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,13 +3,32 @@ import React from 'react'
 import './index.css'
 
 type HeaderProps = {
-  clasName?: string
+  className?: string
   children?: React.ReactNode
 }
 
-export default function Index({ clasName, children }: HeaderProps) {
+/**
+ * `Header` component returns a header with logo and can take additional props.
+ *
+ * @param className optional, used to add more styles for the header.
+ * @param children optional, used to add render children inside the header.
+ *
+ * ```tsx
+ * import Header from '@/components/header'
+ *
+ * <Header />
+ *
+ * // or
+ *
+ *  <Header>
+ *      <Button>JOIN NOW</Button>
+ *      <Button variant="secondary">Sign In</Button>
+ *  </Header>
+ * ```
+ */
+export default function Index({ className, children }: HeaderProps) {
   return (
-    <header className={`header ${clasName}`}>
+    <header className={`header ${className}`}>
       <div>
         <img src="/logo.png" alt="netflix text logo" className="header-img-logo-text" />
         <img src="/logo-n.png" alt="netflix logo" className="header-img-logo-n" />
