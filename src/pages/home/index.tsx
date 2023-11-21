607b50140b348a76b1669cb07bfd794981f63d65
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-21 23:48:30 +0200

    [feat]: added select language button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,11 +5,14 @@ import './index.css'
 import { Link } from 'react-router-dom'
 
 import Button from '@/components/ui/button'
+import LanguageSelector from '@/components/ui/language-selector'
 
 export default function Home() {
   return (
     <div>
       <Header className="max-w">
+        <LanguageSelector />
+
         <Link to={'/discover'}>
           <Button>Sign in</Button>
         </Link>
