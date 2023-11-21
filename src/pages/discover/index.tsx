737e6db42abb6a98f475d6da4f55f441f35e2023
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-21 23:02:49 +0200

    [feat]: updated header width and background and added buttons to it

--- src/pages/discover/index.tsx
+++ src/pages/discover/index.tsx
@@ -2,10 +2,16 @@ import Header from '@/components/header'
 
 import './index.css'
 
+import Button from '@/components/ui/button'
+
 export default function Discover() {
   return (
     <div>
-      <Header className="bg-black"></Header>
+      <Header className="bg-black">
+        <p>UNLIMITED TV SHOWS & MOVIES</p>
+        <Button>JOIN NOW</Button>
+        <Button variant="secondary">SIGN IN</Button>
+      </Header>
     </div>
   )
 }
