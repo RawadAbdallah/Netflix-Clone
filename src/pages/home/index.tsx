53bfe57b04aabca4a24010e34fbba373d6d24d25
RawadAbdallah <rawad0ne@gmail.com>
2023-11-22 03:41:00 +0200

    [feat] Adding Section component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
-import Header from '@/components/header'
-
 import './index.css'
 
 import GettingStartedHero from '@/components/getting-started-hero'
+import Header from '@/components/header'
 import { Link } from 'react-router-dom'
 
+// ui-imports
 import Button from '@/components/ui/button'
 import LanguageSelector from '@/components/ui/language-selector'
+import Section from '@/components/ui/section'
 
 export default function Home() {
   return (
@@ -20,6 +21,25 @@ export default function Home() {
       </Header>
 
       <GettingStartedHero />
+      <Section
+        title="Enjoy on your TV"
+        description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        imgSrc="/images/getting-started/tv.png"
+        isReversed={false}
+      />
+      <Section
+        title="Enjoy on your TV"
+        description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        imgSrc="/images/getting-started/mobile-0819.jpg"
+        isReversed={true}
+      />
+
+      <Section
+        title="Enjoy on your TV"
+        description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        imgSrc="/images/getting-started/device-pile.png"
+        isReversed={false}
+      />
     </div>
   )
 }
