35d6b3cdb1fbdae1f1639d5307984cd4a348eaea
RawadAbdallah <rawad0ne@gmail.com>
2023-11-22 03:27:25 +0200

    [fix] hero background image src url

--- src/components/getting-started-hero/index.tsx
+++ src/components/getting-started-hero/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
     <>
       <div className="getting-started-container">
         <div className="hero-background">
-          <img src="/src/assets/images/getting-started/hero.jpg" alt="hero" />
+          <img src="/images/getting-started/hero.jpg" alt="hero" />
           <div className="hero-overlay"></div>
         </div>
         <div className="getting-started-hero">
