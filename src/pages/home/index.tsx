a9d222f5a98d5e8d1acbabf4a7349b83baa60885
RawadAbdallah <rawad0ne@gmail.com>
2023-11-22 14:35:10 +0200

    [core]: Added a section and changed text for each one

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -32,18 +32,25 @@ export default function Home() {
       />
 
       <Section
-        title="Enjoy on your TV"
-        description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        title="Download your shows to watch offline"
+        description="Save your favorites easily and always have something to watch."
         imgSrc="/images/getting-started/mobile-0819.jpg"
         isReversed={true}
       />
 
       <Section
-        title="Enjoy on your TV"
-        description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        title="Watch everywhere"
+        description="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
         imgSrc="/images/getting-started/device-pile.png"
         bgVideo="/videos/video-devices.m4v"
         clasName="custom-video"
+      />
+
+      <Section
+        title="Create profiles for kids"
+        description="Send kids on adventures with their favorite characters in a space made just for them—free with your membership."
+        imgSrc="/images/getting-started/kids.png"
+        isReversed={true}
       />
 
       <Faq />
