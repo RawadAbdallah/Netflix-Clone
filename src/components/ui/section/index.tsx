728dc24353757638577f5e3f9ad616fa6f042182
RawadAbdallah <rawad0ne@gmail.com>
2023-11-22 02:16:06 +0200

    [docs] Adding documentation

--- src/components/ui/section/index.tsx
+++ src/components/ui/section/index.tsx
@@ -7,6 +7,18 @@ interface myProps {
   isReversed: boolean
 }
 
+/**
+ * `Section()` returns Collapse component.
+ *
+ * It takes title, description, imgSrc and isReversed as parameters.
+ * @param title, a string which is the component's title and displayed in a h1 tag.
+ * @param description, a string which is the component's description.
+ * @param imgSrc, a string to an Image Source url.
+ * @param isReversed, a boolean which determines either the section is reversed or not.
+ *
+ * @returns a custom section tsx tag
+ */
+
 const index: React.FC<myProps> = ({ title, description, imgSrc, isReversed = false }) => {
   const sectionClassName: string = 'section-container ' + (isReversed ? 'reverse' : '')
 
