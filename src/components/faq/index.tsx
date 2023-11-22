e05deac1837f4f6cc775bde3232b4c09c2daa3d6
RawadAbdallah <rawad0ne@gmail.com>
2023-11-22 03:47:18 +0200

    [core] Faq component skeleton

--- src/components/faq/index.tsx
+++ src/components/faq/index.tsx
@@ -1,13 +1,14 @@
 import './index.css'
 
 import CollapseWrapper from '@/components/ui/collapse-wrapper'
+import EmailInput from '@/components/ui/email-input'
 
 function index() {
   return (
     <div className="faq-container">
       <h1 className="faq-title">Frequently Asked Questions</h1>
       <CollapseWrapper />
-      {/* To Do: Add email input component */}
+      <EmailInput />
     </div>
   )
 }
