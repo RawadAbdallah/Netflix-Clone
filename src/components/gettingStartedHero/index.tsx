a9492fad11dd419a881e7c93d7dfe4d1b404b5f0
RawadAbdallah <rawad0ne@gmail.com>
2023-11-22 03:16:18 +0200

    [core] GettingStartedHero Component Skeleton

---
+++ src/components/gettingStartedHero/index.tsx
@@ -0,0 +1,27 @@
+import './index.css'
+
+import EmailInput from '@/components/ui/email-input'
+
+const index = () => {
+  return (
+    <>
+      <div className="getting-started-container">
+        <div className="hero-background">
+          <img src="/src/assets/images/getting-started/hero.jpg" alt="hero" />
+          <div className="hero-overlay"></div>
+        </div>
+        <div className="getting-started-hero">
+          <div className="getting-started-content-container">
+            <h1 className="getting-started-content-title">
+              Unlimited movies, TV shows, and more
+            </h1>
+            <p>Watch anywhere. Cancel anytime.</p>
+            <EmailInput />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default index
