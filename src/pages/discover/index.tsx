a05bd29936d5d44b6a1f2986932a0258ad886532
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-22 13:16:39 +0200

    [feat]: render genre

--- src/pages/discover/index.tsx
+++ src/pages/discover/index.tsx
@@ -2,11 +2,14 @@ import Header from '@/components/header'
 
 import './index.css'
 
+import { fetchTMDB } from '@/utilities'
 import { Link } from 'react-router-dom'
 
 import Button from '@/components/ui/button'
 
 export default function Discover() {
+  const { globalGenres } = fetchTMDB
+
   return (
     <div>
       <Header className="bg-black">
@@ -22,13 +25,21 @@ export default function Discover() {
       </Header>
 
       <main className="discover-main">
-        <div className='discover-main-hero'>
+        <section className="discover-main-hero">
           <h1>Movies</h1>
           <p>
             Movies move us like nothing else can, whether they’re scary, funny, dramatic,
             romantic or anywhere in-between. So many titles, so much to experience.
           </p>
-        </div>
+        </section>
+
+        <section className="discover-movies-by-genre">
+          {globalGenres.map((genre, index) => {
+            if (index < 14) {
+              return <span>{genre}</span>
+            }
+          })}
+        </section>
       </main>
     </div>
   )
